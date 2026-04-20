c69d7f323e0f0369fa52742a992fd522f57382c0
lucasluizbrs <lucasluizbjwv@gmail.com>
2026-04-20 12:19:12 -0300

    Implementa Models, Factories e Seeders para popular a biblioteca

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,25 +1,18 @@
 <?php
-
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Factory as FakerFactory;
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        FakerFactory::create()->unique(true);
+        $this->call([
+            CategorySeeder::class,
+            AuthorPublisherBookSeeder::class,
         ]);
     }
 }
+
